739ac4366655b885312ce2bc59c97c468d2a6cee
dragonflyor <xiaozhe_hb@qq.com>
2016-01-07 19:32:07 +0800

    用F4的FATFS工程做模板

--- STM32_NC_F4/USER/main.c
+++ STM32_NC_F4/USER/main.c
@@ -2,76 +2,93 @@
 #include "delay.h"
 #include "usart.h"
 #include "led.h"
-
-//ALIENTEK 探索者STM32F407开发板 实验1
-//跑马灯实验 -库函数版本
+#include "lcd.h"
+#include "key.h"  
+#include "sram.h"   
+#include "malloc.h" 
+#include "usmart.h"  
+#include "sdio_sdcard.h"    
+#include "malloc.h" 
+#include "w25qxx.h"    
+#include "ff.h"  
+#include "exfuns.h"    
+//ALIENTEK 探索者STM32F407开发板 实验39
+//FATFS 实验 -库函数版本
 //技术支持：www.openedv.com
-//淘宝店铺：http://eboard.taobao.com  
-//广州市星翼电子科技有限公司  
-//作者：正点原子 @ALIENTEK
+//淘宝店铺：http://eboard.taobao.com
+//广州市星翼电子科技有限公司    
+//作者：正点原子 @ALIENTEK 
+ 
 
 int main(void)
-{ 
- 
-	delay_init(168);		  //初始化延时函数
-	LED_Init();		        //初始化LED端口
+{        
+ 	u32 total,free;
+	u8 t=0;	
+	u8 res=0;	
+
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
+	delay_init(168);  //初始化延时函数
+	uart_init(115200);		//初始化串口波特率为115200
+	LED_Init();					//初始化LED 
+	usmart_dev.init(84);		//初始化USMART
+ 	LCD_Init();					//LCD初始化  
+ 	KEY_Init();					//按键初始化 
+	W25QXX_Init();				//初始化W25Q128
+	my_mem_init(SRAMIN);		//初始化内部内存池 
+	my_mem_init(SRAMCCM);		//初始化CCM内存池
 	
-  /**下面是通过直接操作库函数的方式实现IO控制**/	
-	
-	while(1)
+ 	POINT_COLOR=RED;//设置字体为红色 
+	LCD_ShowString(30,50,200,16,16,"Explorer STM32F4");	
+	LCD_ShowString(30,70,200,16,16,"FATFS TEST");	
+	LCD_ShowString(30,90,200,16,16,"ATOM@ALIENTEK");
+	LCD_ShowString(30,110,200,16,16,"2014/5/15");   
+	LCD_ShowString(30,130,200,16,16,"Use USMART for test");	   
+ 	while(SD_Init())//检测不到SD卡
 	{
-	GPIO_ResetBits(GPIOF,GPIO_Pin_9);  //LED0对应引脚GPIOF.9拉低，亮  等同LED0=0;
-	GPIO_SetBits(GPIOF,GPIO_Pin_10);   //LED1对应引脚GPIOF.10拉高，灭 等同LED1=1;
-	delay_ms(500);  		   //延时300ms
-	GPIO_SetBits(GPIOF,GPIO_Pin_9);	   //LED0对应引脚GPIOF.0拉高，灭  等同LED0=1;
-	GPIO_ResetBits(GPIOF,GPIO_Pin_10); //LED1对应引脚GPIOF.10拉低，亮 等同LED1=0;
-	delay_ms(500);                     //延时300ms
+		LCD_ShowString(30,150,200,16,16,"SD Card Error!");
+		delay_ms(500);					
+		LCD_ShowString(30,150,200,16,16,"Please Check! ");
+		delay_ms(500);
+		LED0=!LED0;//DS0闪烁
 	}
-}
-
-
-/**
-*******************下面注释掉的代码是通过 位带 操作实现IO口控制**************************************
-	
-int main(void)
-{ 
- 
-	delay_init(168);		  //初始化延时函数
-	LED_Init();		        //初始化LED端口
-  while(1)
+ 	exfuns_init();							//为fatfs相关变量申请内存				 
+  	f_mount(fs[0],"0:",1); 					//挂载SD卡 
+ 	res=f_mount(fs[1],"1:",1); 				//挂载FLASH.	
+	if(res==0X0D)//FLASH磁盘,FAT文件系统错误,重新格式化FLASH
 	{
-     LED0=0;			  //LED0亮
-	   LED1=1;				//LED1灭
-		 delay_ms(500);
-		 LED0=1;				//LED0灭
-		 LED1=0;				//LED1亮
-		 delay_ms(500);
-	 }
-}
-**************************************************************************************************
- **/	
-	
-/**
-*******************下面注释掉的代码是通过 直接操作寄存器 方式实现IO口控制**************************************
-int main(void)
-{ 
- 
-	delay_init(168);		  //初始化延时函数
-	LED_Init();		        //初始化LED端口
+		LCD_ShowString(30,150,200,16,16,"Flash Disk Formatting...");	//格式化FLASH
+		res=f_mkfs("1:",1,4096);//格式化FLASH,1,盘符;1,不需要引导区,8个扇区为1个簇
+		if(res==0)
+		{
+			f_setlabel((const TCHAR *)"1:ALIENTEK");	//设置Flash磁盘的名字为：ALIENTEK
+			LCD_ShowString(30,150,200,16,16,"Flash Disk Format Finish");	//格式化完成
+		}else LCD_ShowString(30,150,200,16,16,"Flash Disk Format Error ");	//格式化失败
+		delay_ms(1000);
+	}													    
+	LCD_Fill(30,150,240,150+16,WHITE);		//清除显示			  
+	while(exf_getfree("0",&total,&free))	//得到SD卡的总容量和剩余容量
+	{
+		LCD_ShowString(30,150,200,16,16,"SD Card Fatfs Error!");
+		delay_ms(200);
+		LCD_Fill(30,150,240,150+16,WHITE);	//清除显示			  
+		delay_ms(200);
+		LED0=!LED0;//DS0闪烁
+	}													  			    
+ 	POINT_COLOR=BLUE;//设置字体为蓝色	   
+	LCD_ShowString(30,150,200,16,16,"FATFS OK!");	 
+	LCD_ShowString(30,170,200,16,16,"SD Total Size:     MB");	 
+	LCD_ShowString(30,190,200,16,16,"SD  Free Size:     MB"); 	    
+ 	LCD_ShowNum(30+8*14,170,total>>10,5,16);				//显示SD卡总容量 MB
+ 	LCD_ShowNum(30+8*14,190,free>>10,5,16);					//显示SD卡剩余容量 MB			    
 	while(1)
 	{
-     GPIOF->BSRRH=GPIO_Pin_9;//LED0亮
-	   GPIOF->BSRRL=GPIO_Pin_10;//LED1灭
-		 delay_ms(500);
-     GPIOF->BSRRL=GPIO_Pin_9;//LED0灭
-	   GPIOF->BSRRH=GPIO_Pin_10;//LED1亮
-		 delay_ms(500);
+		t++; 
+		delay_ms(200);		 			   
+		LED0=!LED0;
+	} 
+}
 
-	 }
- }	 
-**************************************************************************************************
-**/	
- 
+
 
 
 
